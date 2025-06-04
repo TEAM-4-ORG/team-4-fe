0a427f148c609d9dab558c9b29def07a842322cd
jieun <suspen064@gmail.com>
2025-06-05 02:23:10 +0900

    feat: add framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import useUserIdCheck from '@/hooks/useUserIdCheck';
 import CalculateMansae from '@/components/calculateMansae';
 import { ChatLayout } from '@/components/layout/ChatLayout';
 import { UserSelector } from '@/components/init/userSelector';
+import { motion } from 'motion/react';
 
 export default function HomePage() {
   const router = useRouter();
@@ -22,10 +23,16 @@ export default function HomePage() {
     <ChatLayout>
       <div className='m-10 flex h-full flex-col justify-center gap-20'>
         <div className='text-center text-xl whitespace-pre'>
-          <p>
-            {`반갑습니다. 사주/타로 도서와 openAI 기반으로 \n정확한 사주/타로 해석을 제공하는 000서비스입니다.`}
-          </p>
-          <p>{`모든 응답은 도서를 기반으로 하며, 출처와 함께 제공됩니다.`}</p>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: 'easeOut', duration: 2 }}
+          >
+            <p>
+              {`반갑습니다. 사주/타로 도서와 openAI 기반으로 \n정확한 사주/타로 해석을 제공하는 000서비스입니다.`}
+            </p>
+            <p>{`모든 응답은 도서를 기반으로 하며, 출처와 함께 제공됩니다.`}</p>
+          </motion.div>
           <br />
           <div className='flex w-full justify-center gap-10'>
             <div className='w-full rounded-2xl bg-white shadow-xl'>
