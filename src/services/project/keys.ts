cbd5b15a800fbd5db3a9c19d2eef2dcc3f9896de
jieun <suspen064@gmail.com>
2025-06-06 23:40:55 +0900

    feat: 누락 api 추가

--- src/services/project/keys.ts
+++ src/services/project/keys.ts
@@ -1,5 +1,7 @@
 export const projectInfoKey = (id: number) =>
   ['project', 'projectInfo', id] as const;
 
+export const newProjectKey = () => ['project', 'newProject'] as const;
+
 export const deleteProjectKey = (id: number) =>
   ['project', 'deleteProject', id] as const;
